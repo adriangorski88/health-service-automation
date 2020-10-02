19a5ea6c159ed798df81b1b0514f18dc4f6f4eb5
Adrian Górski <adrian.gorski@britenet.com.pl>
2020-10-02 12:46:56 +0200

    Add method for opening HealthService

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
 public class BaseTest {
+
+    @Test
+    public void openHealthServiceWebApp() {
+
+        String driverPath = "C:\\Users\\BRITENET\\Desktop\\Google Drive\\FILES\\TRENING\\MZ\\12\\Selenium\\src\\test\\resources\\executables\\drivers\\geckodriver.exe";
+        System.setProperty("webdriver.gecko.driver",driverPath);
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://localhost:8080/");
+    }
 }
