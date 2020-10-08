2381cce63d369c6011fd5cd06981021d9243170a
Adrian Górski <adrian.gorski@britenet.com.pl>
2020-10-08 14:36:48 +0200

    Change @BeforeMethod annotations

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,24 +2,23 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
-    protected WebDriver driver;
+    WebDriver driver;
 
-    @BeforeTest
+    @BeforeMethod
     public void setUp() {
 
         String driverPath = "src/test/resources/executables/drivers/geckodriver.exe";
-        System.setProperty("webdriver.gecko.driver",driverPath);
+        System.setProperty("webdriver.gecko.driver", driverPath);
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
     }
 
-    @AfterTest
+    @AfterMethod
     public void tearDown() {
         driver.quit();
     }
